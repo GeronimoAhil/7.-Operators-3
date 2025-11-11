c799b7759ffdf85e14b2e977d1475016f4c93b52
GeronimoAhil <geromy2014@gmail.com>
2025-11-11 18:02:31 +0800

    Add files via upload

---
+++ acp2.py
@@ -0,0 +1,13 @@
+a = 97
+d = 100
+g = 103
+j = 106
+i=int(input("Enter your char to find your ASCII value!"))
+if("char = a"):
+    print("Your ASCII value is 97")
+elif("char = d"):
+    print("Your ASCII value is 100")
+elif("char = g"):
+    print("Your ASCII value is 103")
+elif("char = j"):
+    print("Your ASCII value is 106")
